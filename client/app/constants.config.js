be4ff155c6160bfa4fe1fed8e98f145a7dab3558
Daniel Poinsignon <Daniel Poinsignon>
2017-03-12 17:42:24 +0100

    Testing prod environment

---
+++ client/app/constants.config.js
@@ -0,0 +1,5 @@
+angular.module('app')
+
+.constant('config', {PUBNUB_SUBSCRIBE_KEY:'sub-c-85f3205a-a687-11e6-80fa-02ee2ddab7fe',PUBNUB_PUBLISH_KEY:'pub-c-e0e8d03c-7d9a-450d-89a2-6302369c1996',GITHUB_CLIENT_ID:'16c7ec882715c526f00f',GITHUB_REDIRECT_URI:'http://localhost:9000/',GITHUB_ACCESS_TOKEN_REQUEST_URL:'http://localhost:3000/auth/github',SERVER_URL:'http://localhost:3000/'})
+
+;
